26b905d3082e72ac2a5acd6fe9ab8a011021f029
justinjeong <justin.jeong5@gmail.com>
2020-12-18 19:52:04 +0900

    add: key for repeated components

--- client/src/components/views/MovieDetailPage/Sections/MovieDescription.js
+++ client/src/components/views/MovieDetailPage/Sections/MovieDescription.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Descriptions, Tag } from 'antd'
+import { Descriptions, Tag, Tooltip } from 'antd'
 import { useSelector } from 'react-redux'
 
 function MovieDescription() {
@@ -27,15 +27,21 @@ function MovieDescription() {
     <>
       <Content >
         <Descriptions size="large" column={3}>
-          <Descriptions.Item span={2} label="제목">{currentMovie.title}</Descriptions.Item>
-          <Descriptions.Item label="분류">{getMovieTag()}</Descriptions.Item>
-          <Descriptions.Item label="개봉일">{currentMovie.release_date}</Descriptions.Item>
-          <Descriptions.Item label="런타임">{`${currentMovie.runtime}분`}</Descriptions.Item>
-          <Descriptions.Item label="개봉">{currentMovie.status}</Descriptions.Item>
-          <Descriptions.Item label="후기개수">{currentMovie.vote_count}</Descriptions.Item>
-          <Descriptions.Item label="인기도">{currentMovie.popularity}</Descriptions.Item>
-          <Descriptions.Item label="평점">{currentMovie.vote_average}</Descriptions.Item>
-          <Descriptions.Item label=""> <div style={{ width: '97%', margin: '1rem auto' }}> {currentMovie.overview}</div></Descriptions.Item>
+          <Descriptions.Item key='title' span={2} label="제목">{currentMovie.title}</Descriptions.Item>
+          <Descriptions.Item key='sort' label="분류">{getMovieTag()}</Descriptions.Item>
+          <Descriptions.Item key='released' label="개봉일">{currentMovie.release_date}</Descriptions.Item>
+          <Descriptions.Item key='runtime' label="런타임">{`${currentMovie.runtime}분`}</Descriptions.Item>
+          <Descriptions.Item key='status' label="개봉">{currentMovie.status}</Descriptions.Item>
+          <Descriptions.Item key='reviewCount' label="후기개수">{currentMovie.vote_count}</Descriptions.Item>
+          <Descriptions.Item key='popularity' label="인기도">
+            <Tooltip
+              title={<span>인기도는 다음과 같은 지표를 참고하여 산출합니다. <br />
+                    좋아요가 눌린 수, 조회수, 영상목록에 담긴 수, 전날의 인기도</span>}>
+              {currentMovie.popularity}
+            </Tooltip>
+          </Descriptions.Item>
+          <Descriptions.Item key='rate' label="평점">{currentMovie.vote_average}</Descriptions.Item>
+          <Descriptions.Item key='description' label=""> <div style={{ width: '97%', margin: '1rem auto' }}> {currentMovie.overview}</div></Descriptions.Item>
         </Descriptions>
       </Content>
     </>
