c56188a179678edac450970310d7bb795f10e462
justinjeong <justin.jeong5@gmail.com>
2020-12-19 12:20:49 +0900

    add: page goes up when FavoritePage is loaded

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -11,12 +11,15 @@ import { IMAGE_URL } from '../../../Config'
 const { Title } = Typography;
 
 function FavoritePage() {
-
   const dispatch = useDispatch();
   const { currentUser } = useSelector(state => state.user)
   const { favoritedList, loadFavoritedListLoading, loadFavoritedListDone, changeFavoriteDone, changeFavoriteLoading } = useSelector(state => state.favorite)
+  let movieDetailsPageFrontRef = React.createRef();
 
   useEffect(() => {
+    if (movieDetailsPageFrontRef) {
+      movieDetailsPageFrontRef.scrollIntoView();
+    }
     if (currentUser || changeFavoriteDone) {
       dispatch({
         type: LOAD_FAVORITED_LIST_REQUEST,
@@ -47,43 +50,46 @@ function FavoritePage() {
   }
 
   return (
-    <div style={{ width: '80%', margin: '1rem auto' }}>
-      <Title level={2} style={{ margin: '5rem 0 auto' }} >내가 좋아하는 영화들</Title>
-      <Divider />
-      {(loadFavoritedListLoading || changeFavoriteLoading) && <LoadingPage />}
-      {loadFavoritedListDone && <List
-        itemLayout="vertical"
-        size="large"
-        dataSource={favoritedList}
-        renderItem={item => (
-          <List.Item
-            key={item.title}
-            actions={[
-              <IconText icon={<StarOutlined />} text={item.movieRate} key="list-vertical-star-o" />,
-              <IconText icon={<HeartTwoTone twoToneColor="#eb2f96" onClick={handleFavorite(item)} />} text="56" key="list-vertical-like-o" />,
-              <IconText icon={<MessageOutlined />} text="2" key="list-vertical-message" />,
-            ]}
-            extra={
-              <Card
-                hoverable
-                style={{ borderRadius: 10, maxHeight: 240, height: 'auto', width: 'auto', }}
-                cover={<img
-                  alt="moviePoster"
-                  src={`${IMAGE_URL}/w500${item.moviePoster}`}
+    <>
+      <div ref={node => (movieDetailsPageFrontRef = node)} />
+      <div style={{ width: '80%', margin: '1rem auto' }}>
+        <Title level={2} style={{ margin: '5rem 0 auto' }} >내가 좋아하는 영화들</Title>
+        <Divider />
+        {(loadFavoritedListLoading || changeFavoriteLoading) && <LoadingPage />}
+        {loadFavoritedListDone && <List
+          itemLayout="vertical"
+          size="large"
+          dataSource={favoritedList}
+          renderItem={item => (
+            <List.Item
+              key={item.title}
+              actions={[
+                <IconText icon={<StarOutlined />} text={item.movieRate} key="list-vertical-star-o" />,
+                <IconText icon={<HeartTwoTone twoToneColor="#eb2f96" onClick={handleFavorite(item)} />} text="56" key="list-vertical-like-o" />,
+                <IconText icon={<MessageOutlined />} text="2" key="list-vertical-message" />,
+              ]}
+              extra={
+                <Card
+                  hoverable
                   style={{ borderRadius: 10, maxHeight: 240, height: 'auto', width: 'auto', }}
-                />} />
-            }
-          >
-            <List.Item.Meta
-              avatar={<Avatar size='large' src={`${IMAGE_URL}/w500${item.movieImage}`} />}
-              title={<Link to={`/movie/${item.movieId}`}>{item.movieTitle}</Link>}
-              description={item.movieOriginalTitle}
-            />
-            { item.movieDescription}
-          </List.Item >
-        )}
-      />}
-    </div >
+                  cover={<img
+                    alt="moviePoster"
+                    src={`${IMAGE_URL}/w500${item.moviePoster}`}
+                    style={{ borderRadius: 10, maxHeight: 240, height: 'auto', width: 'auto', }}
+                  />} />
+              }
+            >
+              <List.Item.Meta
+                avatar={<Avatar size='large' src={`${IMAGE_URL}/w500${item.movieImage}`} />}
+                title={<Link to={`/movie/${item.movieId}`}>{item.movieTitle}</Link>}
+                description={item.movieOriginalTitle}
+              />
+              { item.movieDescription}
+            </List.Item >
+          )}
+        />}
+      </div >
+    </>
   )
 }
 
